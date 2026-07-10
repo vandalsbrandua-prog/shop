6713cfd5f83818e465a9e0a6ce5b75c8fe95ce3d
vandalsbrandua-prog <vandals.brand.ua@gmail.com>
2026-07-11 00:21:14 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,24 @@ const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
 
-    if(window.scrollY > 30){
-
+    if (window.scrollY > 30) {
         header.classList.add("header-scroll");
-
-    }else{
-
+    } else {
         header.classList.remove("header-scroll");
-
     }
+
+});
+
+const hero = document.querySelector(".hero-object");
+
+document.addEventListener("mousemove",(e)=>{
+
+    if(!hero) return;
+
+    const x=(e.clientX-window.innerWidth/2)/40;
+    const y=(e.clientY-window.innerHeight/2)/40;
+
+    hero.style.transform=
+    `translate(${-x}px,${-y}px)`;
 
 });
