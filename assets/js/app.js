d33f363c5f369effa505723b40e01be627d077e5
vandalsbrandua-prog <vandals.brand.ua@gmail.com>
2026-07-10 23:53:09 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
+const header = document.querySelector(".header");
 
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 30){
+
+        header.classList.add("header-scroll");
+
+    }else{
+
+        header.classList.remove("header-scroll");
+
+    }
+
+});
